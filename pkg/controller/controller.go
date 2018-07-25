cadf71a2f7c8d226e246dfd56d11faf2ba9e7421
Marko Luksa <marko.luksa@gmail.com>
2018-07-25 12:03:35 +0200

    Refactor: Extract controller_base.go from controller.go

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -18,24 +18,12 @@ package controller
 
 import (
 	"fmt"
-	"time"
 
 	"github.com/golang/glog"
 	appsv1 "k8s.io/api/apps/v1"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/apimachinery/pkg/util/runtime"
-	"k8s.io/apimachinery/pkg/util/wait"
-	kubeinformers "k8s.io/client-go/informers"
-	"k8s.io/client-go/kubernetes"
-	"k8s.io/client-go/kubernetes/scheme"
-	typedcorev1 "k8s.io/client-go/kubernetes/typed/core/v1"
-	appslisters "k8s.io/client-go/listers/apps/v1"
-	corelisters "k8s.io/client-go/listers/core/v1"
-	"k8s.io/client-go/tools/cache"
-	"k8s.io/client-go/tools/record"
-	"k8s.io/client-go/util/workqueue"
 
 	"encoding/json"
 	"k8s.io/apimachinery/pkg/labels"
@@ -44,7 +32,6 @@ import (
 	"strings"
 )
 
-const controllerAgentName = "statefulset-drain-controller"
 const AnnotationStatefulSet = "statefulsets.kubernetes.io/drainer-pod-owner" // TODO: can we replace this with an OwnerReference with the StatefulSet as the owner?
 const AnnotationDrainerPodTemplate = "statefulsets.kubernetes.io/drainer-pod-template"
 
@@ -61,213 +48,6 @@ const (
 	MessageDrainPodDeleted  = "delete Drain Pod %s in StatefulSet %s successful"
 	MessagePVCDeleted       = "delete Claim %s in StatefulSet %s successful"
 )
-
-type Controller struct {
-	// kubeclientset is a standard kubernetes clientset
-	kubeclientset kubernetes.Interface
-
-	statefulSetLister  appslisters.StatefulSetLister
-	statefulSetsSynced cache.InformerSynced
-	pvcLister          corelisters.PersistentVolumeClaimLister
-	pvcsSynched        cache.InformerSynced
-	podLister          corelisters.PodLister
-	podsSynced         cache.InformerSynced
-
-	// workqueue is a rate limited work queue. This is used to queue work to be
-	// processed instead of performing it as soon as a change happens. This
-	// means we can ensure we only process a fixed amount of resources at a
-	// time, and makes it easy to ensure we are never processing the same item
-	// simultaneously in two different workers.
-	workqueue workqueue.RateLimitingInterface
-	// recorder is an event recorder for recording Event resources to the
-	// Kubernetes API.
-	recorder record.EventRecorder
-}
-
-// NewController returns a new sample controller
-func NewController(
-	kubeclientset kubernetes.Interface,
-	kubeInformerFactory kubeinformers.SharedInformerFactory) *Controller {
-
-	// obtain references to shared index informers for the Deployment and Foo
-	// types.
-	statefulSetInformer := kubeInformerFactory.Apps().V1().StatefulSets()
-	pvcInformer := kubeInformerFactory.Core().V1().PersistentVolumeClaims()
-	podInformer := kubeInformerFactory.Core().V1().Pods()
-
-	// Create event broadcaster
-	// Add statefulset-drain-controller types to the default Kubernetes Scheme so Events can be
-	// logged for statefulset-drain-controller types.
-	glog.V(4).Info("Creating event broadcaster")
-	eventBroadcaster := record.NewBroadcaster()
-	eventBroadcaster.StartLogging(glog.Infof)
-	eventBroadcaster.StartRecordingToSink(&typedcorev1.EventSinkImpl{Interface: kubeclientset.CoreV1().Events("")})
-	recorder := eventBroadcaster.NewRecorder(scheme.Scheme, corev1.EventSource{Component: controllerAgentName})
-
-	controller := &Controller{
-		kubeclientset:      kubeclientset,
-		statefulSetLister:  statefulSetInformer.Lister(),
-		statefulSetsSynced: statefulSetInformer.Informer().HasSynced,
-		pvcLister:          pvcInformer.Lister(),
-		pvcsSynched:        pvcInformer.Informer().HasSynced,
-		podLister:          podInformer.Lister(),
-		podsSynced:         podInformer.Informer().HasSynced,
-		workqueue:          workqueue.NewNamedRateLimitingQueue(workqueue.DefaultControllerRateLimiter(), "StatefulSets"),
-		recorder:           recorder,
-	}
-
-	glog.Info("Setting up event handlers")
-	// Set up an event handler for when Foo resources change
-	statefulSetInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: controller.enqueueStatefulSet,
-		UpdateFunc: func(old, new interface{}) {
-			controller.enqueueStatefulSet(new)
-		},
-	})
-	// Set up an event handler for when Pod resources change. This
-	// handler will lookup the owner of the given Pod, and if it is
-	// owned by a StatefulSet resource will enqueue that StatefulSet
-	// resource for processing. This way, we don't need to implement
-	// custom logic for handling Pod resources. More info on this pattern:
-	// https://github.com/kubernetes/community/blob/8cafef897a22026d42f5e5bb3f104febe7e29830/contributors/devel/controllers.md
-	podInformer.Informer().AddEventHandler(cache.ResourceEventHandlerFuncs{
-		AddFunc: controller.handlePod,
-		UpdateFunc: func(old, new interface{}) {
-			newPod := new.(*corev1.Pod)
-			oldPod := old.(*corev1.Pod)
-			if newPod.ResourceVersion == oldPod.ResourceVersion {
-				// Periodic resync will send update events for all known Pods.
-				// Two different versions of the same Pod will always have different RVs.
-				return
-			}
-			controller.handlePod(newPod)
-		},
-		DeleteFunc: controller.handlePod,
-	})
-
-	return controller
-}
-
-// Run will set up the event handlers for types we are interested in, as well
-// as syncing informer caches and starting workers. It will block until stopCh
-// is closed, at which point it will shutdown the workqueue and wait for
-// workers to finish processing their current work items.
-func (c *Controller) Run(threadiness int, stopCh <-chan struct{}) error {
-	defer runtime.HandleCrash()
-	defer c.workqueue.ShutDown()
-
-	// Start the informer factories to begin populating the informer caches
-	glog.Info("Starting StatefulSet scaledown cleanup controller")
-
-	// Wait for the caches to be synced before starting workers
-	glog.Info("Waiting for informer caches to sync")
-	if ok := cache.WaitForCacheSync(stopCh, c.statefulSetsSynced, c.podsSynced); !ok {
-		return fmt.Errorf("failed to wait for caches to sync")
-	}
-
-	glog.Info("Starting workers")
-	for i := 0; i < threadiness; i++ {
-		go wait.Until(c.runWorker, time.Second, stopCh)
-	}
-
-	glog.Info("Started workers")
-	<-stopCh
-	glog.Info("Shutting down workers")
-
-	return nil
-}
-
-// runWorker is a long-running function that will continually call the
-// processNextWorkItem function in order to read and process a message on the
-// workqueue.
-func (c *Controller) runWorker() {
-	for c.processNextWorkItem() {
-	}
-}
-
-// processNextWorkItem will read a single work item off the workqueue and
-// attempt to process it, by calling the syncHandler.
-func (c *Controller) processNextWorkItem() bool {
-	obj, shutdown := c.workqueue.Get()
-
-	if shutdown {
-		return false
-	}
-
-	// We wrap this block in a func so we can defer c.workqueue.Done.
-	err := func(obj interface{}) error {
-		// We call Done here so the workqueue knows we have finished
-		// processing this item. We also must remember to call Forget if we
-		// do not want this work item being re-queued. For example, we do
-		// not call Forget if a transient error occurs, instead the item is
-		// put back on the workqueue and attempted again after a back-off
-		// period.
-		defer c.workqueue.Done(obj)
-		var key string
-		var ok bool
-		// We expect strings to come off the workqueue. These are of the
-		// form namespace/name. We do this as the delayed nature of the
-		// workqueue means the items in the informer cache may actually be
-		// more up to date that when the item was initially put onto the
-		// workqueue.
-		if key, ok = obj.(string); !ok {
-			// As the item in the workqueue is actually invalid, we call
-			// Forget here else we'd go into a loop of attempting to
-			// process a work item that is invalid.
-			c.workqueue.Forget(obj)
-			runtime.HandleError(fmt.Errorf("expected string in workqueue but got %#v", obj))
-			return nil
-		}
-		// Run the syncHandler, passing it the namespace/name string of the
-		// Foo resource to be synced.
-		if err := c.syncHandler(key); err != nil {
-			return fmt.Errorf("error syncing '%s': %s", key, err.Error())
-		}
-		// Finally, if no error occurs we Forget this item so it does not
-		// get queued again until another change happens.
-		c.workqueue.Forget(obj)
-		glog.V(4).Infof("Successfully processed '%s'", key)
-		return nil
-	}(obj)
-
-	if err != nil {
-		runtime.HandleError(err)
-		return true
-	}
-
-	return true
-}
-
-// syncHandler compares the actual state with the desired, and attempts to
-// converge the two. It then updates the Status block of the StatefulSet resource
-// with the current status of the resource.
-func (c *Controller) syncHandler(key string) error {
-
-	glog.V(4).Infof("--------------------------------------------------------------------")
-	glog.V(4).Infof("SyncHandler invoked for %s", key)
-
-	// Convert the namespace/name string into a distinct namespace and name
-	namespace, name, err := cache.SplitMetaNamespaceKey(key)
-	if err != nil {
-		runtime.HandleError(fmt.Errorf("invalid resource key: %s", key))
-		return nil
-	}
-
-	// Get the StatefulSet resource with this namespace/name
-	sts, err := c.statefulSetLister.StatefulSets(namespace).Get(name)
-	if err != nil {
-		// The StatefulSet may no longer exist, in which case we stop
-		// processing.
-		if errors.IsNotFound(err) {
-			runtime.HandleError(fmt.Errorf("StatefulSet '%s' in work queue no longer exists", key))
-			return nil
-		}
-
-		return err
-	}
-
-	return c.processStatefulSet(sts)
-}
 
 func (c *Controller) processStatefulSet(sts *appsv1.StatefulSet) error {
 	// TODO: think about scale-down during a rolling upgrade
@@ -425,82 +205,6 @@ func isDrainPod(pod *corev1.Pod) bool {
 	return pod != nil && pod.ObjectMeta.Annotations[AnnotationStatefulSet] != ""
 }
 
-// enqueueStatefulSet takes a StatefulSet resource and converts it into a namespace/name
-// string which is then put onto the work queue. This method should *not* be
-// passed resources of any type other than StatefulSet.
-func (c *Controller) enqueueStatefulSet(obj interface{}) {
-	var key string
-	var err error
-	if key, err = cache.MetaNamespaceKeyFunc(obj); err != nil {
-		runtime.HandleError(err)
-		return
-	}
-	c.workqueue.AddRateLimited(key)
-}
-
-// handlePod will take any resource implementing metav1.Object and attempt
-// to find the StatefulSet resource that 'owns' it. It does this by looking at the
-// objects metadata.ownerReferences field for an appropriate OwnerReference.
-// It then enqueues that StatefulSet resource to be processed. If the object does not
-// have an appropriate OwnerReference, it will simply be skipped.
-func (c *Controller) handlePod(obj interface{}) {
-
-	if !c.cachesSynced() {
-		return
-	}
-
-	var object metav1.Object
-	var ok bool
-	if object, ok = obj.(metav1.Object); !ok {
-		tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
-		if !ok {
-			runtime.HandleError(fmt.Errorf("error decoding object, invalid type"))
-			return
-		}
-		object, ok = tombstone.Obj.(metav1.Object)
-		if !ok {
-			runtime.HandleError(fmt.Errorf("error decoding object tombstone, invalid type"))
-			return
-		}
-		glog.V(4).Infof("Recovered deleted object '%s' from tombstone", object.GetName())
-	}
-	glog.V(5).Infof("Processing object: %s", object.GetName())
-
-	stsNameFromAnnotation := object.GetAnnotations()[AnnotationStatefulSet]
-	if stsNameFromAnnotation != "" {
-		glog.V(5).Infof("Found pod with '%s' annotation pointing to StatefulSet '%s'. Enqueueing StatefulSet.", AnnotationStatefulSet, stsNameFromAnnotation)
-		sts, err := c.statefulSetLister.StatefulSets(object.GetNamespace()).Get(stsNameFromAnnotation)
-		if err != nil {
-			glog.V(4).Infof("Error retrieving StatefulSet %s: %s", stsNameFromAnnotation, err)
-			return
-		}
-
-		c.enqueueStatefulSet(sts)
-		return
-	}
-
-	if ownerRef := metav1.GetControllerOf(object); ownerRef != nil {
-		// If this object is not owned by a StatefulSet, we should not do anything more
-		// with it.
-		if ownerRef.Kind != "StatefulSet" {
-			return
-		}
-
-		sts, err := c.statefulSetLister.StatefulSets(object.GetNamespace()).Get(ownerRef.Name)
-		if err != nil {
-			glog.V(4).Infof("ignoring orphaned object '%s' of StatefulSet '%s'", object.GetSelfLink(), ownerRef.Name)
-			return
-		}
-
-		c.enqueueStatefulSet(sts)
-		return
-	}
-}
-
-func (c *Controller) cachesSynced() bool {
-	return true // TODO do we even need this?
-}
-
 func newPod(sts *appsv1.StatefulSet, ordinal int) (*corev1.Pod, error) {
 
 	podTemplateJson := sts.Annotations[AnnotationDrainerPodTemplate]
@@ -571,4 +275,8 @@ func extractNameAndOrdinal(pvcName string) (string, int, error) {
 		return "", 0, fmt.Errorf("PVC not created by a StatefulSet")
 	}
 	return name, ordinal, nil
+}
+
+func getStatefulSetNameFromPodAnnotation(object metav1.Object) string {
+	return object.GetAnnotations()[AnnotationStatefulSet]
 }
